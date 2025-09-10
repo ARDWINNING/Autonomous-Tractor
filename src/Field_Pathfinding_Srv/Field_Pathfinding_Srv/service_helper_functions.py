6c55990c1a9c7196e0fb24ec63a597af76c785f7
Alexander Winning <ardwinning@gmail.com>
2025-09-09 23:12:24 -0400

    Added modularity and implementation for clockwise and anitclockwise turn loops as well as for height smoothing for all turn types. Created a list combining function that handles both backfilling and frontfilling depending on the slope of the hill.

--- src/Field_Pathfinding_Srv/Field_Pathfinding_Srv/service_helper_functions.py
+++ src/Field_Pathfinding_Srv/Field_Pathfinding_Srv/service_helper_functions.py
@@ -1,17 +1,20 @@
 from typing import NamedTuple
+from typing import List
 import json
-from math import atan, cos, sin, tan, pi
+from math import acos, atan, cos, sin, tan, pi
 
 class point_object(NamedTuple):
     x: float
     y: float
 
-def load_json(field_id):
-    with open(str(field_id)&'.json', 'r') as file:
+def load_json(field_id: int):
+    with open(str(field_id)+'.json', 'r') as file:
+        
         data = json.load(file)
+    return data
 
 
-def end_of_field_pos(arrable_multiplier, implement_width, points): 
+def end_of_field_pos(arrable_multiplier: float, implement_width: float, points: list[point_object]) -> point_object: 
     """
      # For the input arr of points you can extrapolate the points to the extremities of the implement width using
      # trigonometry as it is a line from last point to point in screen(case of both off just add 2 at both extremities)
@@ -40,54 +43,133 @@ def end_of_field_pos(arrable_multiplier, implement_width, points):
             if(current_error_value > error_value):
                 error_value = current_error_value
                 optimal_height = trial_height
+    if optimal_height is None:
+        optimal_height = float(points[0].y)
     return point_object(x = (points[0].x + 0.5*implement_width), y = optimal_height)
 
-def end_of_field_turn(implement_width, turning_radius, points):
+def combine_points(reverse: bool, master_points: list[point_object], insertion_points: list[point_object], insertion_index: int) -> int:
+    for point in insertion_points:    
+        master_points.insert(insertion_index, point)
+        if(not reverse):
+            insertion_index += 1
+    return insertion_index                            
+
+def clockwise_turn_loop(start_angle: float, end_angle: float, granularity: int, turn_points: list[point_object], turning_radius: float, polarity: tuple[bool, bool, bool]):
+    increment = pi/granularity
+    turn_angle = start_angle + increment
+    pol_x, pol_y, add_turn = polarity
+    while turn_angle <= end_angle:
+        if(turn_angle < pi/2):
+            aug_x = (turning_radius - turning_radius * cos(turn_angle)) * 1 if pol_x else -1
+            aug_y = (turning_radius * sin(turn_angle)) * 1 if pol_y else -1
+            turn_points.append(point_object(x = (turn_points[0].x + aug_x), y = (turn_points[0].y + aug_y)))
+        else:
+            aug_x = (turning_radius if add_turn else 0 + (turning_radius * sin(turn_angle - pi/2))) * 1 if pol_x else -1
+            aug_y = (turning_radius - turning_radius * cos(turn_angle - pi/2)) * 1 if pol_y else -1
+            turn_points.append(point_object(x = (turn_points[0].x + aug_x), y = (turn_points[0].y + aug_y)))
+        if((turn_angle != end_angle) and ((turn_angle + increment) > end_angle)):
+            turn_angle = end_angle
+        else:
+            turn_angle += increment
+    return turn_points
+
+def anticlockwise_turn_loop(start_angle: float, end_angle: float, granularity: int, turn_points: list[point_object], turning_radius: float, polarity: tuple[bool, bool, bool]):
+    increment = pi / granularity
+    turn_angle = start_angle - increment
+    pol_x, pol_y, add_turn = polarity
+    while turn_angle >= end_angle:
+        if turn_angle > -pi / 2:
+            aug_x = (turning_radius - turning_radius * cos(turn_angle)) * (1 if pol_x else -1)
+            aug_y = (turning_radius * sin(turn_angle)) * (1 if pol_y else -1)
+            turn_points.append(point_object(x=(turn_points[0].x + aug_x), y=(turn_points[0].y + aug_y)))
+        else:
+            aug_x = ((turning_radius if add_turn else 0) + (turning_radius * sin(turn_angle + pi / 2))) * (1 if pol_x else -1)
+            aug_y = (turning_radius - turning_radius * cos(turn_angle + pi / 2)) * (1 if pol_y else -1)
+            turn_points.append(point_object(x=(turn_points[0].x + aug_x), y=(turn_points[0].y + aug_y)))
+        if (turn_angle != end_angle) and ((turn_angle - increment) < end_angle):
+            turn_angle = end_angle
+        else:
+            turn_angle -= increment
+    return turn_points
+
+def turn_height_smoothing(granularity: int, theta: float, turning_disparity: float, turning_radius: float, points: list[point_object], insertion_index: int = 1) -> int:
+    swarth_height_offset = points[insertion_index].y-points[insertion_index-1].y
+    if(turning_disparity >= 0):
+        unrecoverable_height_offset = abs(swarth_height_offset) - turning_disparity*tan(theta)
+        if((swarth_height_offset >= 0) and (unrecoverable_height_offset !=0)):
+            points.insert(insertion_index, point_object(x = (points[insertion_index-1].x), y = (points[insertion_index-1].y + unrecoverable_height_offset)))
+            insertion_index += 1
+        elif(unrecoverable_height_offset !=0):
+            points.insert(insertion_index, point_object(x = (points[insertion_index].x), y = (points[insertion_index].y + unrecoverable_height_offset)))
+        return insertion_index
+    standard_turn_width = 2*turning_radius*(1-cos(pi/4))
+    if((turning_disparity + standard_turn_width) < 0):
+        phi = acos(1+ turning_disparity/(2*turning_radius))
+        temp_list: list[point_object] = []
+        if(swarth_height_offset >= 0):
+            temp_list.append(points[insertion_index-1])
+            anticlockwise_turn_loop(0, phi, granularity, temp_list, turning_radius, (False, True, False))
+            temp_list.remove(temp_list[0])
+            insertion_index = combine_points(False, points, temp_list, insertion_index)
+            temp_list = [points[insertion_index-1]]
+            clockwise_turn_loop(pi-phi, pi, granularity, temp_list, turning_radius, (False, True, False))
+            temp_list.remove(temp_list[0])
+            insertion_index = combine_points(False, points, temp_list, insertion_index)
+        else:
+            temp_list.append(points[insertion_index])
+            clockwise_turn_loop(0, phi, granularity, temp_list, turning_radius, (True, True, False))
+            temp_list.remove(temp_list[0])
+            insertion_index = combine_points(True, points, temp_list, insertion_index)
+            temp_list = [points[insertion_index]]
+            anticlockwise_turn_loop(pi-phi, pi, granularity, temp_list, turning_radius, (True, True, False))
+            temp_list.remove(temp_list[0])
+            insertion_index = combine_points(True, points, temp_list, insertion_index)
+    else:
+        temp_list: list[point_object] = []
+        length = (turning_disparity + standard_turn_width)/cos(pi/4)
+        aug_y = length * sin(pi/4)
+        if(swarth_height_offset >= 0):
+            temp_list.append(points[insertion_index-1])
+            anticlockwise_turn_loop(0, pi/4, granularity, temp_list, turning_radius, (False, True, False))
+            temp_list.remove(temp_list[0])
+            insertion_index = combine_points(False, points, temp_list, insertion_index)
+            aug_x = -length * cos(pi/4)
+            points.insert(insertion_index, point_object(x = (points[insertion_index-1].x + aug_x), y = (points[insertion_index-1].y + aug_y)))
+            insertion_index += 1
+            temp_list = [points[insertion_index-1]]
+            clockwise_turn_loop(pi-pi/4, pi, granularity, temp_list, turning_radius, (False, True, False))
+            temp_list.remove(temp_list[0])
+            insertion_index = combine_points(False, points, temp_list, insertion_index)
+        else:
+            temp_list.append(points[insertion_index])
+            clockwise_turn_loop(0, pi/4, granularity, temp_list, turning_radius, (True, True, False))
+            temp_list.remove(temp_list[0])
+            insertion_index = combine_points(True, points, temp_list, insertion_index)
+            aug_x = length * cos(pi/4)
+            points.insert(insertion_index, point_object(x = (points[insertion_index].x + aug_x), y = (points[insertion_index].y + aug_y)))
+            temp_list = [points[insertion_index]]
+            anticlockwise_turn_loop(pi-pi/4, pi, granularity, temp_list, turning_radius, (True, True, False))
+            temp_list.remove(temp_list[0])
+            insertion_index = combine_points(True, points, temp_list, insertion_index)
+
+    insertion_index = turn_height_smoothing(granularity, atan(abs(points[insertion_index].y - points[insertion_index-1].y)/abs(points[insertion_index].x - points[insertion_index-1].x)), 0, turning_radius, points, insertion_index)
+    return insertion_index
+
+def bounded_turn(granularity: int,  insertion_index: int, theta: float, turning_radius: float, points: list[point_object]):
+    if(points[insertion_index].y < points[insertion_index-1].y):
+        phi = theta
+    else:
+        phi = pi/2 + theta
+    phi += 1 #avoids unused error will finish this
+    return points  
+
+def end_of_field_turn(implement_width: float, turning_radius: float, points: list[point_object]) -> List[point_object]: # For under field where smaller y is away from field wrap the whole call in a transformer that takes points and rectifies the numbers and undoes at the end by making all input y/x negative?
     """
      # The points input is a list consisting of the output of e_o_f_pos for the end of swarth a and beginning of swarth b and theta 2 is the ang
      # This will then determine the correct turn and produce a list of points to follow for the turn 
     """
-    downward = False
-    swarth_height_offset = points[1].y-points[0].y
-    if(swarth_height_offset < 0):
-        downward = True
     turning_disparity = implement_width - 2 * turning_radius
-    running_index = 1
-    granularity = pi/16
-    if(turning_disparity >= 0):
-        theta = atan((abs(swarth_height_offset))/(implement_width)) 
-        if(downward):
-            initial_run = swarth_height_offset + (turning_disparity*tan(theta))
-            points.insert(running_index, point_object(x = (points[0].x + implement_width), y = (points[0].y + initial_run)))
-            phi = pi/2 + theta
-        else:
-            initial_run = swarth_height_offset - (turning_disparity*tan(theta))
-            points.insert(running_index, point_object(x = points[0].x, y = (points[0].y + initial_run)))
-            running_index += 1
-            phi = pi/2 - theta 
-        for turn_angle in range(0, phi, granularity):
-            if(turn_angle < pi/2):
-                aug_x = turning_radius - turning_radius * cos(turn_angle)
-                aug_y = turning_radius * sin(turn_angle)
-            else:  
-                aug_x = turning_radius + turning_radius * cos(turn_angle - pi/2)
-                aug_y = turning_radius * sin(turn_angle - pi/2)
-            points.insert(running_index, point_object(x = (points[0].x + aug_x), y = (points[0].y + aug_y)))
-            running_index += 1
-        if(turning_disparity != 0):
-            aug_x = 0.5 * turning_disparity * cos(theta)
-            aug_y = 0.5 * turning_disparity * sin(theta) * (-1 if downward else 1)
-            points.insert(running_index, point_object(x = (points[0].x + aug_x), y = (points[0].y + aug_y)))
-        for turn_angle in range(phi, pi, granularity):
-            if(turn_angle < pi/2):
-                aug_x = turning_radius - turning_radius * cos(turn_angle)
-                aug_y = turning_radius * sin(turn_angle)
-            else:
-                aug_x = turning_radius + turning_radius * cos(turn_angle - pi/2)
-                aug_y = turning_radius * sin(turn_angle - pi/2)
-            points.insert(running_index, point_object(x = (points[0].x + aug_x), y = (points[0].y + aug_y)))
-            running_index += 1
-    else:
-        # Todo: write the bowed out turn
-        pass
+    theta = atan((abs(points[1].y - points[0].y))/(implement_width)) 
+    running_index = turn_height_smoothing(16, theta, turning_disparity, turning_radius, points) # Look at code to ensure all good with the augmented passing structure
+    bounded_turn(16, running_index, theta, turning_radius, points)
     return points
